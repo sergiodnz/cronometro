639d3fd54f458782afd1dd447152e047cb38e2cd
sergiodnz <sergiodnz@gmail.com>
2021-01-03 16:27:21 -0300

    configuracao do script de upload

--- src/components/Upload/api.js
+++ src/components/Upload/api.js
@@ -1,7 +1,7 @@
 import server from '../../config/server';
 
 export const doUpload = (formData) => {
-    return server.post('doupload.php', formData, {
+    return server.post('doupload', formData, {
         headers: {
          "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
         }
--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost/aovivo/',
+  baseURL: 'http://localhost/',
   timeout: 1000 * 120,
 });
 
